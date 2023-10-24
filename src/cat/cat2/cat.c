a9be26ad167b1b0afb1eeb1ff3bdec5d62c37c2f
Medusa Galadriel <medusaga@il-d4.msk.21-school.ru>
2023-10-24 05:14:45 +0300

    safe

--- src/cat/cat2/cat.c
+++ src/cat/cat2/cat.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 #include "parse_flags.h"
 
+
 int main(int argc, char *argv[]) {
     FILE *fp;
     int line_index = 0;
     int previous = 0;
     int current = 0;
     int extra_line = 0;
-
+int count=1;
     flags Flag = parse_flags(argc, argv);
     fp = fopen(argv[argc - 1], "r");
-
+int x;
     int c;
     while (1) {
         c = fgetc(fp);
         current = c;
         if (feof(fp)) break;
-
+//x=(previous == 10 && current == 10);
+//printf("X=%d!", x);
         if (previous == 10 && current == 10) {
-            extra_line = 1;
+            count++;
+            if(count>2){
+            count--;
+             continue;}
         }
+
+
+
+        /*{
+            extra_line = 1;
+        }*/
 
         
 
-        if (Flag.s == 1 && previous == 10 && current == 10 && extra_line == 1) {
-            continue;
-        }
-        if (current != 10) {
+        //if (Flag.s == 1 && previous == 10 && current == 10 && extra_line == 1) {
+            
+            
+       // }
+        //if (current != 10) {
             //if (extra_line == 1) {printf("\n");}
-            extra_line = 0;
-        }
+           // extra_line = 0;
+       // }
 
         printf("%c", c);
         previous = c;
---
+++ src/cat/cat2/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int x=1;
+    if(x==2)
+    printf("1");
+    printf("2");
+}
