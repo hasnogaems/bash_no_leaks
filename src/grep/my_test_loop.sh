beead55c1daf9e63e12c3bed7fd145121521d87b
hasnogaems <nickpolom@mail.ru>
2023-11-16 18:03:02 +0300

    tests

--- src/grep/my_test_loop.sh
+++ src/grep/my_test_loop.sh
@@ -4,10 +4,10 @@ SUCCESS=0
 FAIL=0
 
 if [ $# -eq 0 ]; then
-for i in v e11 c
+for i in e11
 do
 #diff -s <(cat -$i debug_n.txt) <(./s21_cat  debug_n.txt) 
-d="$(diff -s <(grep -$i debug_e.txt) <(./a.out -$i debug_e.txt))"
+d="$(diff -s <(grep -$i test_e.txt) <(./s21_grep -$i test_e.txt))"
 ((COUNTER++))
 if [ "$d" == "Files /dev/fd/63 and /dev/fd/62 are identical" ]
 then
@@ -24,14 +24,14 @@ fi
 
 done
 # 2 flags
-for flag1 in v e11 c
+for flag1 in e11
 do
     for flag2 in v e11 c 
     do
         if [ $flag1 != $flag2 ]
         then
         combo2="-$flag1$flag2"
-        dcombo2="$(diff -s <(cat $combo2 test.txt) <(./s21_cat $combo2 test.txt))"
+        dcombo2="$(diff -s <(grep -$flag1 -$flag2 test_e.txt) <(./s21_grep $combo2 test_e.txt))"
         if [ "$dcombo2" == "Files /dev/fd/63 and /dev/fd/62 are identical" ]
 then
 ((SUCCESS++))
@@ -49,6 +49,6 @@ echo -e "\e[1m\e[32m$SUCCESS \e[93msuccesses/\e[91m$FAIL \e[97mfails"
 
 
 elif [ $# -gt 0 ]; then
-diff -s <(cat $1 debug_n.txt) <(./s21_cat $1  debug_n.txt)
+diff -s <( $1 debug_n.txt) <(./s21_cat $1  debug_n.txt)
 fi
 
