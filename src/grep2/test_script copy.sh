bff5586f0ff3ca33c562f679ba974a3d3b107efe
hasnogaems <nickpolom@mail.ru>
2023-11-24 16:52:41 +0300

    before recover stash

--- src/grep2/test_script copy.sh
+++ src/grep2/test_script copy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Укажите путь к  программе
 PATH_PROGRAM="./s21_grep"
@@ -6,7 +6,7 @@ PATH_PROGRAM="./s21_grep"
 GREP="grep"
 
 # Папка с тестовыми файлами
-TEST_DIR=""
+TEST_DIR="../common/test_files"
 # Папка для результатов  программы
 OUTPUT_DIR="../common/test_grep/output"
 # Папка для результатов утилиты grep
@@ -17,25 +17,23 @@ mkdir -p "$OUTPUT_DIR"
 mkdir -p "$GREP_OUTPUT_DIR"
 
 FLAGS_TO_TEST=("-e" "-i" "-v" "-c" "-l" "-n" "-h" "-s" "-o")
-FILE="test.txt"
+FILE="text.txt"
 PATTERN="t..t"
 
 for FLG in "${FLAGS_TO_TEST[@]}"
 do
     # Тестируем программу
-    echo "$PATH_PROGRAM $FLG $PATTERN \"$FILE\" > \"$OUTPUT_DIR/$FILE\""
-    $PATH_PROGRAM $FLG $PATTERN "$FILE" > "$OUTPUT_DIR/$FILE"
+    $PATH_PROGRAM $FLG $PATTERN "$TEST_DIR/$FILE" > "$OUTPUT_DIR/$FILE"
 
     # Тестируем утилиту grep
-    echo "$GREP $FLG $PATTERN \"$FILE\" > \"$GREP_OUTPUT_DIR/$FILE\""
-    $GREP $FLG $PATTERN "$FILE" > "$GREP_OUTPUT_DIR/$FILE"
+    $GREP $FLG $PATTERN "$TEST_DIR/$FILE" > "$GREP_OUTPUT_DIR/$FILE"
 
     # Сравниваем результаты
     diff "$OUTPUT_DIR/$FILE" "$GREP_OUTPUT_DIR/$FILE" > /dev/null
     if [ $? -eq 0 ]; then
-      echo -e "File testing $FILE with flags $FLG -> \e[32msuccess\e[0m"
+      echo "File testing $FILE with flags $FLG -> \033[32msuccess\033[0m"
     else
-      echo -e "File testing $FILE with flags $FLG -> \e[31mfail\e[0m"
+      echo "File testing $FILE with flags $FLG -> \033[31mfail\033[0m"
     fi
     rm -rf "$OUTPUT_DIR/$FILE"
     rm -rf "$GREP_OUTPUT_DIR/$FILE"
@@ -46,19 +44,17 @@ FLG="-f"
 PATTERN="../common/test_files/pattern.txt"
 
 # Тестируем программу
-    echo "$PATH_PROGRAM $FLG $PATTERN \"$FILE\" > \"$OUTPUT_DIR/$FILE\""
-    $PATH_PROGRAM $FLG $PATTERN "$FILE" > "$OUTPUT_DIR/$FILE"
+    $PATH_PROGRAM $FLG $PATTERN "$TEST_DIR/$FILE" > "$OUTPUT_DIR/$FILE"
 
     # Тестируем утилиту grep
-    echo "$GREP $FLG $PATTERN \"$FILE\" > \"$GREP_OUTPUT_DIR/$FILE\""
-    $GREP $FLG $PATTERN "$FILE" > "$GREP_OUTPUT_DIR/$FILE"
+    $GREP $FLG $PATTERN "$TEST_DIR/$FILE" > "$GREP_OUTPUT_DIR/$FILE"
 
     # Сравниваем результаты
     diff "$OUTPUT_DIR/$FILE" "$GREP_OUTPUT_DIR/$FILE" > /dev/null
     if [ $? -eq 0 ]; then
-      echo -e "File testing $FILE with flags $FLG -> \e[32msuccess\e[0m"
+      echo "File testing $FILE with flags $FLG -> \033[32msuccess\033[0m"
     else
-      echo -e "File testing $FILE with flags $FLG -> \e[31mfail\e[0m"
+      echo "File testing $FILE with flags $FLG -> \033[31mfail\033[0m"
     fi
     rm -rf "$OUTPUT_DIR/$FILE"
     rm -rf "$GREP_OUTPUT_DIR/$FILE"
@@ -72,23 +68,23 @@ FLAGS_TO_TEST=("-iv" "-ic" "-in"
 "-vln" "-vlh" "-vls" "-vlo" "-vnh" "-vns" "-vno" "-vhs" "-vho" 
 "-vso" "-cln" "-clh" "-cls" "-clo" "-cnh" "-cns" "-cno" "-chs" 
 "-cho" "-cso" "-lnh" "-lns" "-lno" "-lhs" "-lho" "-lso" "-nhs")
-FILE="test.txt"
+FILE="text.txt"
 PATTERN="t..t"
 
 for FLG in "${FLAGS_TO_TEST[@]}"
 do
     # Тестируем программу
-    $PATH_PROGRAM $FLG $PATTERN "$FILE" > "$OUTPUT_DIR/$FILE"
+    $PATH_PROGRAM $FLG $PATTERN "$TEST_DIR/$FILE" > "$OUTPUT_DIR/$FILE"
 
     # Тестируем утилиту grep
-    $GREP $FLG $PATTERN "$FILE" > "$GREP_OUTPUT_DIR/$FILE"
+    $GREP $FLG $PATTERN "$TEST_DIR/$FILE" > "$GREP_OUTPUT_DIR/$FILE"
 
     # Сравниваем результаты
     diff "$OUTPUT_DIR/$FILE" "$GREP_OUTPUT_DIR/$FILE" > /dev/null
     if [ $? -eq 0 ]; then
-      echo -e "File testing $FILE with flags $FLG -> \e[32msuccess\e[0m"
+      echo "File testing $FILE with flags $FLG -> \033[32msuccess\033[0m"
     else
-      echo -e "File testing $FILE with flags $FLG -> \e[31mfail\e[0m"
+      echo "File testing $FILE with flags $FLG -> \033[31mfail\033[0m"
     fi
     rm -rf "$OUTPUT_DIR/$FILE"
     rm -rf "$GREP_OUTPUT_DIR/$FILE"
@@ -102,48 +98,48 @@ FLAGS_TO_TEST=("-v" "-c" "-n" "-h" "-vc" "-vn"
 "-cns" "-cno" "-cso" "-lnh" "-lns" "-lno" 
 "-lhs" "-lho" "-lso" "-nhs")
 
-FILE="test.txt"
-FILE2="test2.txt"
+FILE="text.txt"
+FILE2="text2.txt"
 PATTERN="t..t"
 
 for FLG in "${FLAGS_TO_TEST[@]}"
 do
     # Тестируем программу
-    $PATH_PROGRAM $FLG $PATTERN "$FILE" "$FILE2" > "$OUTPUT_DIR/$FILE"
+    $PATH_PROGRAM $FLG $PATTERN "$TEST_DIR/$FILE" "$TEST_DIR/$FILE2" > "$OUTPUT_DIR/$FILE"
 
     # Тестируем утилиту grep
-    $GREP $FLG $PATTERN "$FILE" "$FILE2" > "$GREP_OUTPUT_DIR/$FILE"
+    $GREP $FLG $PATTERN "$TEST_DIR/$FILE" "$TEST_DIR/$FILE2" > "$GREP_OUTPUT_DIR/$FILE"
 
     # Сравниваем результаты
     diff "$OUTPUT_DIR/$FILE" "$GREP_OUTPUT_DIR/$FILE" > /dev/null
     if [ $? -eq 0 ]; then
-      echo -e "File testing $FILE and $FILE2 with flags $FLG -> \e[32msuccess\e[0m"
+      echo "File testing $FILE and $FILE2 with flags $FLG -> \033[32msuccess\033[0m"
     else
-      echo -e "File testing $FILE and $FILE2 with flags $FLG -> \e[31mfail\e[0m"
+      echo "File testing $FILE and $FILE2 with flags $FLG -> \033[31mfail\033[0m"
     fi
     rm -rf "$OUTPUT_DIR/$FILE"
     rm -rf "$GREP_OUTPUT_DIR/$FILE"
 done
 
-FILE="test.txt"
-FILE2="test.txt"
+FILE="text.txt"
+FILE2="text2.txt"
 PATTERN="t..t"
 PATTERN2="1"
 
 for FLG in "${FLAGS_TO_TEST[@]}"
 do
     # Тестируем программу
-    $PATH_PROGRAM -e $PATTERN -e $PATTERN2 $FLG "$FILE" "$FILE2" > "$OUTPUT_DIR/$FILE"
+    $PATH_PROGRAM -e $PATTERN -e $PATTERN2 $FLG "$TEST_DIR/$FILE" "$TEST_DIR/$FILE2" > "$OUTPUT_DIR/$FILE"
 
     # Тестируем утилиту grep
-    $GREP -e $PATTERN -e $PATTERN2 $FLG "$FILE" "$FILE2" > "$GREP_OUTPUT_DIR/$FILE"
+    $GREP -e $PATTERN -e $PATTERN2 $FLG "$TEST_DIR/$FILE" "$TEST_DIR/$FILE2" > "$GREP_OUTPUT_DIR/$FILE"
 
     # Сравниваем результаты
     diff "$OUTPUT_DIR/$FILE" "$GREP_OUTPUT_DIR/$FILE" > /dev/null
     if [ $? -eq 0 ]; then
-      echo -e "File testing $FILE and $FILE2 with flags $FLG and templates [$PATTERN], [$PATTERN2] (by flag -e)  -> \e[32msuccess\e[0m"
+      echo "File testing $FILE and $FILE2 with flags $FLG and templates [$PATTERN], [$PATTERN2] (by flag -e)  -> \033[32msuccess\033[0m"
     else
-      echo -e "File testing $FILE and $FILE2 with flags $FLG and templates [$PATTERN], [$PATTERN2] (by flag -e) -> \e[31mfail\e[0m"
+      echo "File testing $FILE and $FILE2 with flags $FLG and templates [$PATTERN], [$PATTERN2] (by flag -e) -> \033[31mfail\033[0m"
     fi
     rm -rf "$OUTPUT_DIR/$FILE"
     rm -rf "$GREP_OUTPUT_DIR/$FILE"
